bbdd93e592832290d4f94501c07a5933f06bf2b4
hypark <phy29230666t@gmail.com>
2025-07-26 12:05:24 +0000

    로컬호스트에서 연결을 거부하였습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="w-screen h-screen overflow-hidden">
       <iframe
-        src="http://localhost:8080"
+        src="/ide"
         className="w-full h-full border-0"
         title="WindWalker IDE"
       />
