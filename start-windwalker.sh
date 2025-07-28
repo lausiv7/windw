02aafca74c32bbcdf9438e7cc2c83405fcbb8b5b
hypark <phy29230666t@gmail.com>
2025-07-28 09:05:02 +0000

    studio-6767631293:~/studio{main}$ ./start-windwalker.sh 🚀 WindWalker Co

--- start-windwalker.sh
+++ start-windwalker.sh
@@ -2,11 +2,9 @@
 echo "🚀 WindWalker Code-Server 시작 중..."
 
 # Code-Server 포그라운드 실행
-# --extra-extensions-dir 옵션을 추가하여 개발 중인 확장 폴더를 지정합니다.
 ~/.local/bin/code-server --bind-addr 0.0.0.0:8081 \
            --user-data-dir ~/.local/share/code-server \
            --extensions-dir ~/.local/share/code-server/extensions \
-           --extra-extensions-dir ~/studio/extensions \
            --disable-telemetry \
            --auth none \
            --log debug \
